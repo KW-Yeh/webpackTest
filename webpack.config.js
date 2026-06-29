5db3a5372f2b70994965e09d4ba7c30dddb3f9a5
kw_yeh <kw_yeh@cyberlink.com>
2026-06-29 15:21:40 +0800

    Add cache-busted build asset names

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const env = (process.env.NODE_ENV || "development");
 const manifestPath = path.resolve(__dirname, 'manifest.json');
 
 class ManifestPlugin {
@@ -26,58 +25,69 @@ class ManifestPlugin {
     }
 }
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'static/js/main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)?$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react"
-                        ],
-                    }
-                }]
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    env === "development" ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
-        ]
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    output: {
-                        comments: false
-                    }
+module.exports = (env, argv = {}) => {
+    const mode = argv.mode || 'development';
+    const isDevelopment = mode === 'development';
+
+    return {
+        mode,
+        entry: './src/index.jsx',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: isDevelopment ? 'static/js/[name].js' : 'static/js/[name].[contenthash:8].js',
+            chunkFilename: isDevelopment ? 'static/js/[name].chunk.js' : 'static/js/[name].[contenthash:8].chunk.js',
+            clean: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)?$/,
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                "@babel/preset-env",
+                                "@babel/preset-react"
+                            ],
+                        }
+                    }]
                 },
-                extractComments: false
-            })
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-        }),
-        new ManifestPlugin(),
-    ],
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ],
+                },
+            ]
+        },
+        devServer: {
+            historyApiFallback: true
+        },
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        output: {
+                            comments: false
+                        }
+                    },
+                    extractComments: false
+                })
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                filename: 'index.html',
+            }),
+            new MiniCssExtractPlugin({
+                filename: isDevelopment ? 'static/css/[name].css' : 'static/css/[name].[contenthash:8].css',
+                chunkFilename: isDevelopment ? 'static/css/[name].chunk.css' : 'static/css/[name].[contenthash:8].chunk.css',
+            }),
+            new ManifestPlugin(),
+        ],
+    };
 };
